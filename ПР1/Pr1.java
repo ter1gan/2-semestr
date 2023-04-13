4a5e0647ee9e2451332f69df9497018541171a34
ter1gan <113279557+ter1gan@users.noreply.github.com>
2023-04-13 21:22:44 +0300

    Update Pr1.java

--- ПР1/Pr1.java
+++ ПР1/Pr1.java
@@ -2,29 +2,20 @@ import java.util.Random;
 
 public class Pr1 {
     public static void main(String[] args) {
-        int[][] m = new int[3][3];
         Random rnd = new Random();
-        for (int i = 0; i < m.length; i++, System.out.println()) {
-            for (int j = 0; j < m[i].length; j++) {
-                m[i][j] = rnd.nextInt(10);
-                System.out.print(m[i][j] + " ");
+        int size1 = 2;
+        int size2 = 4;
 
+        int[][] matrix2 = new int[size1][size2];
+        
+        for (int i = 0; i < size2; i++) {
+            for (int j = 0; j < size1; j++) {
+                matrix2[j][i] = rnd.nextInt(10);
+                System.out.print(matrix2[j][i] + " ");
             }
+            System.out.println();
         }
-        System.out.println(" ");
 
-        for (int i = 0; i < m.length; i++, System.out.println()) {
-            for (int j = 0; j < m[i].length; j++) {
-
-                System.out.print(m[j][i] + " ");
-
-
-
-
-
-
-            }
-        }
 
 
 
