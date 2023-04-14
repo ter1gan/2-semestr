b40d8d0f6b994ddca90967b16a2e9bb37b3cd184
ter1gan <113279557+ter1gan@users.noreply.github.com>
2023-04-14 21:16:12 +0300

    Update Pr2.java

--- ПР2/Pr2.java
+++ ПР2/Pr2.java
@@ -4,19 +4,31 @@ public class Pr2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Для завершения введите значение отличное от 0 и 1");
+        List<String> spisok = new ArrayList<String>();
         var n = in.nextInt();
         int i = 0;
         if (n < 2) {
-            i++;
+            if(n==1){
+                i++;
+                spisok.add("true");
+            } else {
+                spisok.add("false");
+            }
+
         }
         while (n < 2){
             n = in.nextInt();
             if (n == 1){
                 i++;
-            }
+                spisok.add("true");
+            } else {
+                spisok.add("false");
 
+            }
         }
-        System.out.println("Количество работников: " + i);
+        int index = spisok.size() - 1;
+        spisok.remove(index);
+        System.out.println(spisok + "\n" + "Количество работников: " + i);
 
     }
 
