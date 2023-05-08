844febeb248bd21acbf015c65f01de0a3888fc97
ter1gan <113279557+ter1gan@users.noreply.github.com>
2023-05-08 15:40:31 +0300

    Update Pr31.java

--- ПР31/Pr31.java
+++ ПР31/Pr31.java
@@ -7,34 +7,39 @@ public class Pr31 {
         fibonacci(100);
     }
     public static void fibonacci(int n){
-        BigInteger bigInt = BigInteger.ONE;
-        BigInteger bigInt2 = BigInteger.ONE;
-        BigInteger number = BigInteger.ONE;
-        for (int i = 2; i < n;i++) {
-            number = bigInt.add(bigInt2);
-            bigInt2 = bigInt;
-            bigInt = number;
-        }
-        System.out.println(bigInt);
-        int[] intTab = String.valueOf(bigInt).chars().map(Character::getNumericValue).toArray();
-        HashMap<Integer, Integer> counting = new HashMap<>();
-        for(int j : intTab) {
-            if (counting.containsKey(j)){
-                counting.put(j, counting.get(j) + 1);
-            }else{
-                counting.put(j, 1);
+        if( 10 <= n && n <= 100000){
+         
+            BigInteger bigInt = BigInteger.ONE;
+            BigInteger bigInt2 = BigInteger.ONE;
+            BigInteger number = BigInteger.ONE;
+            for (int i = 2; i < n;i++) {
+                number = bigInt.add(bigInt2);
+                bigInt2 = bigInt;
+                bigInt = number;
             }
-        }
-        int maxKey = 0;
-        int maxVal = 0;
-        for (Map.Entry<Integer, Integer> couples : counting.entrySet()) {
-            if(couples.getValue() > maxVal){
-                maxVal = couples.getValue();
+            System.out.println(bigInt);
+            int[] intTab = String.valueOf(bigInt).chars().map(Character::getNumericValue).toArray();
+            HashMap<Integer, Integer> counting = new HashMap<>();
+            for(int j : intTab) {
+                if (counting.containsKey(j)){
+                    counting.put(j, counting.get(j) + 1);
+                }else{
+                    counting.put(j, 1);
+                }
             }
-            if(couples.getValue() == maxVal){
-                maxKey = couples.getKey();
+            int maxKey = 0;
+            int maxVal = 0;
+            for (Map.Entry<Integer, Integer> couples : counting.entrySet()) {
+                if(couples.getValue() > maxVal){
+                    maxVal = couples.getValue();
+                }
+                if(couples.getValue() == maxVal){
+                    maxKey = couples.getKey();
+                }
             }
+            System.out.println(maxKey + " " + maxVal);
+            
         }
-        System.out.println(maxKey + " " + maxVal);
+        
     }
 }
